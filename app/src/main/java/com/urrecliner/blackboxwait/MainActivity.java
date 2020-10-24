979401a6eebad5f32e9eb4983ccc06b84bb898e2
riopapa <riopapa@gmail.com>
2020-10-24 14:01:04 +0900

    Add waiting time up down button

--- app/src/main/java/com/urrecliner/blackboxwait/MainActivity.java
+++ app/src/main/java/com/urrecliner/blackboxwait/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.Timer;
@@ -11,11 +13,27 @@ import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
-    static int delaySecs = 40;
+    static int delaySecs = 42;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageButton vUp = findViewById(R.id.wait_up);
+        vUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                delaySecs += 11;
+            }
+        });
+        final ImageButton vDown = findViewById(R.id.wait_down);
+        vDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                delaySecs -= 5;
+            }
+        });
+
 //        if (getIntent().hasExtra("delay"))
 //            delayTime = getIntent().getIntExtra("delay",4444);
         new Timer().schedule(new TimerTask() {
@@ -24,8 +42,9 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
+                        String str = ""+delaySecs;
                         TextView tv = findViewById(R.id.delayTime);
-                        tv.setText(delaySecs +"");
+                        tv.setText(str);
                     }
                 });
 
@@ -35,8 +54,10 @@ public class MainActivity extends AppCompatActivity {
                     assert sendIntent != null;
                     startActivity(sendIntent);
                     System.exit(0);
+                    android.os.Process.killProcess(android.os.Process.myPid());
                 }
             }
         }, 0, 1000);
     }
+
 }
